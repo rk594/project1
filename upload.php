d7b4cc70385088f65c3f7c27b5e84498813b766e
rachana kanagala <rk594@afsconnect1.njit.edu>
2017-10-22 22:53:39 -0400

    adding php upload file

---
+++ upload.php
@@ -0,0 +1,23 @@
+<?php
+$obj = new upload();
+class upload
+{
+public function __construct()
+{
+if($_SERVER['REQUEST_METHOD'] == 'POST') {
+$this::post();
+}
+}
+public static function post()
+{
+$target_dir = "upload_dir/";
+$tmp_name = $_FILES["fileToUpload"]["tmp_name"];
+$name = basename($_FILES["fileToUpload"]["name"]);
+move_uploaded_file($tmp_name, "$target_dir/$name");
+$sow = $_FILES["fileToUpload"]["name"];
+$s = $target_dir . $sow ;
+$t = "upload_dir/" . $_FILES["fileToUpload"]["name"]; 
+header("Location: csv2html.php?file=$t");
+}
+}
+?>
