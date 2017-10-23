d8d7737f31902ee39e1d7231174e9a1de2b1e4f1
rachana kanagala <rk594@afsconnect1.njit.edu>
2017-10-22 22:37:54 -0400

    adding index file

---
+++ index1.php
@@ -0,0 +1,36 @@
+<?php
+ini_set('display_errors', 'On');
+error_reporting(E_ALL);
+class Manage {
+public static function autoload($class) {
+include $class . '.php';
+echo "rrr";
+}
+}
+spl_autoload_register(array('Manage', 'autoload'));
+$obj = new main();
+echo $obj::$html;
+class main
+{
+public static $html;
+public  function __construct()
+{
+$form =   '<!DOCTYPE html>';
+$form .=   '<html>';
+$form .=   '<head>';
+$form .=   '<style>.error {color: #FF0000;}';
+$form .=   '<link rel="stylesheet" href="styles.css">';
+$form .=   '</style></head><body>';
+$form .=   '<form action="upload.php" method="post"
+enctype="multipart/form-data">';
+$form .=   ' BROWSE THE FILE TO UPLOAD: <br><br>';
+$form .=   ' <input type="file" name="fileToUpload" id="fileToUpload">';
+$form .=   '<br><br>';
+$form .=   ' <br><br> <input type="submit" value="Upload File" name="submit">';
+$form .=   '</form>';
+$form .=   '</body>';
+$form .=   '</html>';
+$this::$html .= $form;
+}
+}
+?>
